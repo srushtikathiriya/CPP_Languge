a86a14041054b6a6e39bc735e8cdeea22d261d0c
srushti <srushtikathiriya2003@gmail.com>
2023-07-07 11:43:25 +0530

    7/7/23

--- oop's program/bank_ac.cpp
+++ oop's program/bank_ac.cpp
@@ -44,7 +44,7 @@ int main(){
         case 0: 
             break;
         default:
-            printf("Wrong Choice...");
+            cout<<"Wrong Choice...";
             break;
     }
     }while(ch!=0);
