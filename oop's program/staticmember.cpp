9b194fe0aa78ea21000ba2cb846f251a08a9fb8b
srushti <srushtikathiriya2003@gmail.com>
2023-07-24 09:48:32 +0530

    hello

---
+++ oop's program/staticmember.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class test{
+    public:
+    static int a;     //declare
+    test()
+    {
+        cout<<"test calling...."<<endl;
+        a++;
+    }
+};
+int test::a=50;    //init
+int main(){
+    cout<<"starting point:"<<test::a<<endl;
+    test t1,t2;
+    cout<<"ending point:"<<test::a<<endl;
+}
